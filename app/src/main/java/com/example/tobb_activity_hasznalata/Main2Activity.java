3a8e06ae51a37fe881fdd7baac60ed957bafe925
japkidzsola <suzukipontzsolt@gmail.com>
2019-09-25 14:40:27 +0200

    fullos program

--- app/src/main/java/com/example/tobb_activity_hasznalata/Main2Activity.java
+++ app/src/main/java/com/example/tobb_activity_hasznalata/Main2Activity.java
@@ -1,4 +1,33 @@
 package com.example.tobb_activity_hasznalata;
 
-public class Main2Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    Button buttonVissza;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2_main);
+
+        buttonVissza = findViewById(R.id.buttonVissza);
+
+        buttonVissza.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent vissza = new Intent(Main2Activity.this, MainActivity.class);
+
+                startActivity(vissza);
+                finish();
+            }
+        });
+
+
+    }
 }
